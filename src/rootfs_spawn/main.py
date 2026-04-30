821cfd7d7ef621a1cc83c0d536cf0ddfa900d5b3
Timo Reichl <thetredev@gmail.com>
2026-05-01 01:09:51 +0200

    implement logging and reformat using ruff
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- src/rootfs_spawn/main.py
+++ src/rootfs_spawn/main.py
@@ -1,3 +1,4 @@
+import logging
 import shutil
 from pathlib import Path
 from typing import Iterator
@@ -9,9 +10,7 @@ PREREQUISITE_MAP: dict[str, list[Path]] = {
     "debian": [
         Path("debootstrap"),
     ],
-    "archlinux": [
-        Path("pacstrap")
-    ]
+    "archlinux": [Path("pacstrap")],
 }
 PREREQUISITE_MAP["ubuntu"] = PREREQUISITE_MAP["debian"]
 
@@ -24,16 +23,50 @@ class PrerequisiteNotExecutableError(Exception):
     pass
 
 
+def create_logger() -> logging.Logger:
+    # create logger
+    logger = logging.getLogger("pretty-logger")
+    logger.setLevel(logging.INFO)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    # create formatter
+    formatter = logging.Formatter(
+        fmt="[rootfs-spawn] %(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    return logger
+
+
+logger = create_logger()
+
+
 def assert_prerequisites(executables: Iterator[Path]):
     for executable in executables:
         resolved_path = executable.as_posix()
 
         if shutil.which(resolved_path) is None:
-            raise PrerequisiteNotExecutableError(f"The command 'which {resolved_path}' has failed!")
+            raise PrerequisiteNotExecutableError(
+                f"The command 'which {resolved_path}' has failed!"
+            )
+
+        logger.info(
+            f"Found prerequisite executable '{executable}' at '{resolved_path}'"
+        )
 
 
 # all args after the * are switches (i.e., -c, --count, -x)
-#greeting: str, y: int, *, count: int = 1, x: str
+# greeting: str, y: int, *, count: int = 1, x: str
+
 
 def cli(distro: str):
     """
@@ -45,8 +78,8 @@ def cli(distro: str):
     if distro not in PREREQUISITE_MAP:
         raise DistroNotSupportedError(distro)
 
-    executables = PREREQUISITE_MAP[distro]
-    assert_prerequisites(iter(executables))
+    executables = iter(PREREQUISITE_MAP[distro])
+    assert_prerequisites(executables)
 
 
 def main():
